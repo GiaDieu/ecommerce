66b8648615c418d6534b51ad5b0d8d62b6b64614
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-11-24 21:34:32 +0700

    add_badge_style

--- client/src/Layout/Header.js
+++ client/src/Layout/Header.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   return (
     <header className="row">
       <div>
@@ -11,6 +14,9 @@ const Header = () => {
       </div>
       <div>
         <Link to="/cart">Cart</Link>
+        {cartItems.length > 0 && (
+          <span className="badge">{cartItems.length}</span>
+        )}
         <Link to="/signin">Sign In</Link>
       </div>
     </header>
