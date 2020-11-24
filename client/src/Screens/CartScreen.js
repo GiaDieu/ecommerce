545acce7e1d8c5d6ee19c4e41f884520e21acd23
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-11-24 21:27:25 +0700

    dispatch_Action_addToCart

--- client/src/Screens/CartScreen.js
+++ client/src/Screens/CartScreen.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { addToCart } from '../Actions/CartActions';
+import { useDispatch } from 'react-redux';
 
 const CartScreen = (props) => {
   const productId = props.match.params.id;
   const qty = props.location.search
     ? Number(props.location.search.split('=')[1])
     : 1;
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
 
   return (
     <div>
