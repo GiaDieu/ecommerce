6969dc189641e7169e89a163138bc70b63d977e4
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-11-24 20:22:01 +0700

    Up_ApiProductDetails

--- server/server.js
+++ server/server.js
@@ -1,17 +1,27 @@
-import express from "express";
-import data from "./data";
+import express from 'express';
+import data from './data';
 
 // in order to run this import (ES6), install some packages npm install @babel/cli @babel/core  @babel/node @babel/preset-env nodemon --save-dev and create the file .babelrc and set up
 const app = express();
 
-app.get("/api/products", async (req, res) => {
-  res.send(data);
+app.get('/api/products', async (req, res) => {
+    res.send(data);
 });
-app.get("/", async (req, res) => {
-  res.send("Server is Ready!");
+
+app.get('/api/products/:id', async (req, res) => {
+    const product = data.products.find((x) => x._id === req.params.id);
+    if (product) {
+        res.send(product);
+    } else {
+        res.status(404).send({ msg: 'Product Not Found!' });
+    }
+});
+
+app.get('/', async (req, res) => {
+    res.send('Server is Ready!');
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Serve at http://localhost:${PORT}`);
+    console.log(`Serve at http://localhost:${PORT}`);
 });
