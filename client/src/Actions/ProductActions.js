dd872437126710498e36f249cd7c4aa1c03423b7
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-11-24 19:27:33 +0700

    test_eslint

--- client/src/Actions/ProductActions.js
+++ client/src/Actions/ProductActions.js
@@ -1,16 +1,16 @@
-import Axios from "axios";
+import Axios from 'axios';
 import {
-  PRODUCT_LIST_FAIL,
-  PRODUCT_LIST_REQUEST,
-  PRODUCT_LIST_SUCCESS,
-} from "../constants/ProductConstants";
+    PRODUCT_LIST_FAIL,
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+} from '../constants/ProductConstants';
 
 export const listProducts = () => async (dispatch) => {
-  dispatch({ type: PRODUCT_LIST_REQUEST });
-  try {
-    const { data } = await Axios.get("/api/products");
-    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data.products });
-  } catch (err) {
-    dispatch({ type: PRODUCT_LIST_FAIL, payload: err.message });
-  }
+    dispatch({ type: PRODUCT_LIST_REQUEST });
+    try {
+        const { data } = await Axios.get('/api/products');
+        dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data.products });
+    } catch (err) {
+        dispatch({ type: PRODUCT_LIST_FAIL, payload: err.message });
+    }
 };
--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Header from "./Layout/Header";
-import HomeScreen from "./Screens/HomeScreen";
-import ProductScreen from "./Screens/ProductScreen";
-import Footer from "./Layout/Footer";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './Layout/Header';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
+import Footer from './Layout/Footer';
 
 const App = () => {
-  return (
-    <Router>
-      <div className="grid-container">
-        <Header />
-        <main>
-          <Route path="/" exact component={HomeScreen} />
-          <Route path="/product/:id" component={ProductScreen} />
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="grid-container">
+                <Header />
+                <main>
+                    <Route path="/" exact component={HomeScreen} />
+                    <Route path="/product/:id" component={ProductScreen} />
+                </main>
+                <Footer />
+            </div>
+        </Router>
+    );
 };
 
 export default App;
