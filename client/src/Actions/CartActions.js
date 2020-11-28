31a1e0d1f3e6f31926d305e7cb32db845c2ca4c2
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-11-28 19:16:24 +0700

    Create_saveShippingAddressAction

--- client/src/Actions/CartActions.js
+++ client/src/Actions/CartActions.js
@@ -1,5 +1,9 @@
 import Axios from 'axios';
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/CartConstants';
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from '../constants/CartConstants';
 
 export const addToCart = (productId, qty) => async (dispatch, getState) => {
   const { data } = await Axios.get(`/api/products/${productId}`);
@@ -20,4 +24,9 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
 export const removeFromCart = (cartItemId) => (dispatch, getState) => {
   dispatch({ type: CART_REMOVE_ITEM, payload: cartItemId });
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
+
+export const saveShippingAddress = (data) => async (dispatch) => {
+  dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data });
+  localStorage.setItem('shippingAddress', JSON.stringify(data));
 };
