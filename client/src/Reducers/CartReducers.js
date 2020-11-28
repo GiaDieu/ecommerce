25809d098dd16b5c25e2c338785504951d8a2dff
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-11-28 19:17:26 +0700

    ADD_CART_SAVE_SHIPPING_ADDRESS_REDUCER

--- client/src/Reducers/CartReducers.js
+++ client/src/Reducers/CartReducers.js
@@ -1,4 +1,8 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/CartConstants';
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from '../constants/CartConstants';
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
@@ -26,6 +30,9 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           (cartItem) => cartItem.id !== action.payload,
         ),
       };
+
+    case CART_SAVE_SHIPPING_ADDRESS:
+      return { ...state, shippingAddress: action.payload };
     default:
       return state;
   }
