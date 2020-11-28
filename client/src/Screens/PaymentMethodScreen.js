7f1a4b530d3d7f7863f5bd3eb0d825700f5c1570
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-11-28 19:53:14 +0700

    ADD_LOGIC - PaymentMethod

---
+++ client/src/Screens/PaymentMethodScreen.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import CheckoutSteps from '../components/CheckoutSteps';
+import { useDispatch } from 'react-redux';
+import { savePaymentMethod } from '../Actions/CartActions';
+
+const PaymentMethodScreen = (props) => {
+  const [paymentMethod, setPaymentMethod] = useState('PayPal');
+  const dispatch = useDispatch();
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    props.history.push('/placeorder');
+  };
+
+  return (
+    <div>
+      <CheckoutSteps step1 step2 step3 />
+      <form className="form" onSubmit={onSubmitHandler}>
+        <div>
+          <h1>Payment Method</h1>
+        </div>
+        <div>
+          <div>
+            <input
+              type="radio"
+              id="paypal"
+              value="PayPal"
+              name="paymentMethod"
+              required
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            />
+            <label htmlFor="paypal">Paypal</label>
+          </div>
+        </div>
+        <div>
+          <div>
+            <input
+              type="radio"
+              id="stripe"
+              value="Stripe"
+              name="paymentMethod"
+              required
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            />
+            <label htmlFor="stripe">Stripe</label>
+          </div>
+        </div>
+        <div>
+          <button className="primary" type="submit">
+            Continue
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default PaymentMethodScreen;
